c7bf43babc2999f0f8317630f81ba202f85eff5c
Teis Jayaswal <teisjayaswal@gmail.com>
2022-11-20 00:12:24 -0800

    Set up home and login pages

---
+++ client/src/components/JobDetails.js
@@ -0,0 +1,46 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
+import { useAuthContext } from "../hooks/useAuthContext"
+import { useJobsContext } from "../hooks/useJobsContext"
+//date-fns
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+
+const JobDetails =({ job }) => {
+  const { dispatch } = useJobsContext()
+  const { user } = useAuthContext()
+  
+  if (!user) {
+    return
+  }
+  const handleClick = async () => {
+    const response = await fetch('/api/jobs/' + job._id, {
+      method: 'DELETE',
+      headers: {
+        'Authorization': `Bearer ${user.token}`
+      }
+    })
+    const json = await response.json()
+
+    if (response.ok) {
+      dispatch({ type: 'DELETE_JOB', payload: json })
+    }
+  }
+  return (
+    <div className="job-details">
+      <h4>{job.title}</h4>
+      <p><strong>{job.company}</strong></p>
+      <a href={job.link}><p><strong>Link to Listing</strong></p></a>
+      <input
+          type="checkbox"
+          value={job.applied}
+          // onChange={handleChange}
+          id="applied"
+          name="applied"
+        />
+      <p><strong>Notes: </strong>{job.notes}</p>
+      <p>{formatDistanceToNow(new Date(job.createdAt), {addSuffix: true })}</p>
+      <span className="material-symbols-outlined" onClick={handleClick}>delete</span>
+    </div>
+  )
+}
+
+export default JobDetails
---
+++ client/src/pages/Home.js
@@ -0,0 +1,42 @@
+import { useEffect } from "react"
+import { useAuthContext } from "../hooks/useAuthContext"
+import { useJobsContext } from "../hooks/useJobsContext"
+//components 
+import JobDetails from "../components/JobDetails"
+import JobForm from "../components/JobForm"
+
+const Home = () => {
+  const {jobs, dispatch} = useJobsContext()
+  const {user} = useAuthContext()
+  useEffect(() => {
+    const fetchJobs = async () => {
+      const response = await fetch('/api/jobs',   {
+        headers: {
+          'Authorization': `Bearer ${user.token}`
+        }
+      })
+      const json = await response.json()
+      
+      if (response.ok) {
+        dispatch({ type: 'SET_JOBS', payload: json})
+      }
+    }
+    if (user) {
+      fetchJobs()
+    }
+
+}, [dispatch, user])
+
+return (
+  <div className="home">
+    <div className="jobs">
+      {jobs && jobs.map((job) => (
+        <><JobDetails key={job.title} job={job} /></>
+      ))} </div>
+      <JobForm />
+  </div>
+)
+  
+}
+
+export default Home
