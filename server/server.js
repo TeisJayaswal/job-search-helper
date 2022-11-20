1139b646007127ae17cc6c1cc1d8cbfacc561c77
Teis Jayaswal <teisjayaswal@gmail.com>
2022-11-19 16:17:05 -0800

    Start of the backend architecture

---
+++ server/server.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-undef */
+const express = require("express");
+const mongoose = require("mongoose");
+
+//express app
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+//connect to db
+mongoose
+  .connect(process.env.MONG_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("connected to db and listening on port", process.env.PORT);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
